4f2a97fad3aebe6525fb6aa8d1504a4a8b3cb16a
junchao.li <junchao.li@tongfudun.com>
2022-08-12 15:27:56 +0800

    init

---
+++ src/test/java/cn/itutopia/itutopia_env/ItutopiaEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itutopia.itutopia_env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItutopiaEnvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
